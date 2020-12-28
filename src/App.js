a799a9b96799b9dbf305f17ff4a5588802925c15
nelo <nelokwaz@gmail.com>
2020-12-27 22:18:13 -1200

    added an onchange event to the textarea to update the state

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import "./App.css";
 export default function App() {
   //the [markdown, setMarkdown]  is a value and function that was provided to us by useState and we use array destructuring to pull them out
   const [markdown, setMarkdown] = useState("# sup");
+
+  function handleChange(e) {
+    setMarkdown(e.target.value);
+  }
+
   return (
     <div className="app">
-      <textarea value={markdown} />
+      <textarea onChange={handleChange} value={markdown} />
 
       <div className="preview">{markdown}</div>
     </div>
