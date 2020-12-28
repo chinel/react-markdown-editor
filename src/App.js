63cb205cb945040f37db7f28edb33cc1f2956f74
nelo <nelokwaz@gmail.com>
2020-12-27 21:34:25 -1200

    stored the value of the text area in the state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 export default function App() {
+  //the [markdown, setMarkdown]  is a value and function that was provided to us by useState and we use array destructuring to pull them out
+  const [markdown, setMarkdown] = useState("# sup");
   return (
     <div className="app">
-      <textarea />
+      <textarea value={markdown} />
 
-      <div className="preview" />
+      <div className="preview">{markdown}</div>
     </div>
   );
 }
